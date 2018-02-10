1006b65346d5fc4f7f790bcd787bde3411033dcf
Kerollmops <renault.cle@gmail.com>
2018-02-10 19:47:05 +0100

    feat(Welder): Add basic methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-#[derive(Debug)]
+use std::iter::once;
+
 pub struct Welder<G, T> {
     glue: G,
     welded: T,
@@ -14,24 +15,63 @@ impl<G, T: Default> Welder<G, T> {
 }
 
 impl<G, T> Welder<G, T> {
+    pub fn from<U>(glue: G, base: &U) -> Self
+    where
+        U: ToOwned + ?Sized,
+        U::Owned: Into<T>
+    {
+        Welder {
+            glue: glue,
+            welded: Into::into(ToOwned::to_owned(base)),
+        }
+    }
+}
+
+impl<G, T> Welder<G, T> {
     pub fn weld(self) -> T {
         self.welded
     }
 }
 
-impl<G, T> Welder<G, T> {
+impl<G, T> Welder<G, T>
+where
+    G: Clone,
+    T: Extend<G>
+{
     pub fn push<E>(&mut self, elem: E) -> &mut Self
     where
-        T: Extend<E> + Extend<G>
+        T: Extend<E>
     {
-        unimplemented!()
+        self.welded.extend(once(self.glue.clone()));
+        self.welded.extend(once(elem));
+
+        self
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::Welder;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn string_welder() {
+        let mut welder = Welder::new(' ');
+
+        welder.push("foo");
+
+        let string: String = welder.weld();
+
+        assert_eq!(" foo", &string);
+    }
+
+    #[test]
+    fn string_welder_from_base() {
+        let mut welder = Welder::from(' ', "foo");
+
+        welder.push("bar");
+
+        let string: String = welder.weld();
+
+        assert_eq!("foo bar", &string);
     }
 }
