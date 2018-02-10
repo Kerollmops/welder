106002d8fd80ddf16f5509c78f3c26f83f837636
Kerollmops <renault.cle@gmail.com>
2018-02-10 18:36:03 +0100

    feat: Add the first skeleton

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,33 @@
+#[derive(Debug)]
+pub struct Welder<G, T> {
+    glue: G,
+    welded: T,
+}
+
+impl<G, T: Default> Welder<G, T> {
+    pub fn new(glue: G) -> Self {
+        Welder {
+            glue: glue,
+            welded: <T as Default>::default(),
+        }
+    }
+}
+
+impl<G, T> Welder<G, T> {
+    pub fn weld(self) -> T {
+        self.welded
+    }
+}
+
+impl<G, T> Welder<G, T> {
+    pub fn push<E>(&mut self, elem: E) -> &mut Self
+    where
+        T: Extend<E> + Extend<G>
+    {
+        unimplemented!()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
